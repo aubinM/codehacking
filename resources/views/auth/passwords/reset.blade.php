544d10b9c291675169ad39d7b81498bc14407998
MATHIEU Aubin <aubin51100@hotmail.fr>
2019-05-14 16:02:45 +0100

    Reset password design + work

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -17,46 +17,56 @@
                             <input type="hidden" name="token" value="{{ $token }}">
 
                             <div class="row">
-                                
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus  placeholder="Email">
 
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                <div class="col-lg-12">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Réinitialisation mot de passe</h1>
+                                        </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                        <div class="form-group">
+                                            <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ $login ?? old('login') }}" required autocomplete="on" autofocus  placeholder="Login">
 
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                            @error('login')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
 
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                        </div>
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Reset Password') }}
-                                    </button>
+                                        <div class="form-group">
+                                            
+
+
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Nouveau mot de passe">
+
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+
+                                        </div>
+
+
+                                        <div class="form-group">
+
+
+
+                                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmation mdp">
+
+                                        </div>
+
+                                        
+                                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                    {{ __('Réinitialisé le mot de passe') }}
+                                                </button>
+                                           
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -64,8 +74,7 @@
                 </div>
             </div>
         </div>
-    </div>
-    @include('includes.bottom_scripts')
+        @include('includes.bottom_scripts')
 
 </body>
 
